a6661a9ed66067071bb8cfd0dddcaa4a995c4f6a
SingleDog <a5457789889409@gmail.com>
2025-12-31 15:27:05 +0800

    feat: testing basic plugin listeners

--- src/main/java/me/practice/practice_server/PracticeServer.java
+++ src/main/java/me/practice/practice_server/PracticeServer.java
@@ -1,13 +1,19 @@
 package me.practice.practice_server;
 
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
 
-public final class PracticeServer extends JavaPlugin {
+public final class PracticeServer extends JavaPlugin implements Listener {
 
   @Override
   public void onEnable() {
     // Plugin startup logic
     getLogger().info("Practice Server Plugin Enabled");
+    getServer().getPluginManager().registerEvents(this, this);
   }
 
   @Override
@@ -16,4 +22,15 @@ public final class PracticeServer extends JavaPlugin {
     getLogger().info("Practice Server Plugin Disabled");
   }
 
+  @EventHandler
+  public void onJoin(PlayerJoinEvent event) {
+    event.getPlayer().sendMessage(
+        String.format("%sWelcome to the practice server, %s", ChatColor.AQUA, event.getPlayer().getName()));
+    getLogger().info(String.format("Sent welcome message to player %s", event.getPlayer().getName()));
+  }
+
+  @EventHandler
+  public void onPlayerDropItem(PlayerDropItemEvent event) {
+    event.setCancelled(true);
+  }
 }
